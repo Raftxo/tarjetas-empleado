6d2cb3ebdcb64e08a24a6ab2949ea3c2a9f1bce9
Raftxo <fatwhiteduke@gmail.com>
2026-06-04 20:42:21 +0200

    update app.jsx, app.css, add banner

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ function App() {
     <>
     <main className='contenedor-principal'>
       <header className='cabecera-app'>
+        {/* Solución: 1. La imagen de public se llama directamente con una barra (/) */}
+        <img src="/banner.jpeg" alt="Banner de empleados" className='imagen-banner'/>
         <h1>Directorio de empleados</h1>
         <p>Aprende React usando arquitectura correcta.</p>
         </header>
@@ -16,19 +18,19 @@ function App() {
         <section className='galeria-empleados'>
           {/* Reutilización Máxima: pasamos props */}
           <UserCard 
-            nombre='Ana Garcia'
+            nombre='Ana García'
             profesion='Frontend Developer'
             imagen='https://randomuser.me/api/portraits/women/3.jpg'
           />
 
           <UserCard 
-            nombre='Juan Perez'
+            nombre='Juan Pérez'
             profesion='UX/UI Designer'
             imagen='https://randomuser.me/api/portraits/men/3.jpg'
           />
 
           <UserCard 
-            nombre='Luisa Rodriguez'
+            nombre='Luisa Rodríguez'
             profesion='DevOps Engineer'
             imagen='https://randomuser.me/api/portraits/women/44.jpg'
           />
